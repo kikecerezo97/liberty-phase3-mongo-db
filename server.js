808ff5864ef3f699096c8501ba99a88eeae7e973
n1563871 <Luis.Gonzalez02@LibertyFianzas.com>
2024-04-24 17:09:28 -0600

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-
-// server.js file
 const express = require('express');
 const path = require('path');  // for handling file paths
+const da = require("./data-access");
 
 const app = express();
 const port = process.env.PORT || 4000;  // use env var or default to 4000
 
 // Set the static directory to serve files from
-app.use(express.static(path.join(__dirname, 'public')));
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
+    console.log("staticDir: " + staticDir);
+});
+
+app.get("/customers", async (req, res) => {
+    const cust = await da.getCustomers();
+    res.send(cust);
 });
